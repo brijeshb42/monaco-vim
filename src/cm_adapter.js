07352f0bdba5ff16ba33b983e89777a5ba6547b4
Bernard Xie <bxie94@gmail.com>
2022-04-01 03:05:33 -0700

    Fix smart indent typo (#97)
    * fix auto indent typo
    
    * Update src/cm_adapter.js
    
    Co-authored-by: Anmol Sethi <97054713+nhooyr-ts@users.noreply.github.com>
    
    Co-authored-by: Anmol Sethi <97054713+nhooyr-ts@users.noreply.github.com>

--- src/cm_adapter.js
+++ src/cm_adapter.js
@@ -1257,7 +1257,7 @@ class CMAdapter {
   }
 
   smartIndent() {
-    editor.getAction("editor.action.formatSelection").run();
+    this.editor.getAction("editor.action.reindentselectedlines").run();
   }
 
   moveCursorTo(to) {
